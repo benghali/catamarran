03181880a0dcdbada822175f0353ecef4bf4722e
Achref <113175845+benghali@users.noreply.github.com>
2022-10-13 14:04:08 +0100

    Add files via upload

---
+++ catamarran.js
@@ -0,0 +1,15 @@
+var paragraph=document.getElementById("description");
+function myFunction(){
+    return paragraph;
+}
+function VisitorData(fisrtName,lastName,mail,paymentMode){
+var visitor={};
+visitor.fisrtName=fisrtName,
+visitor.lastName=lastName,
+visitor.mail=mail,
+visitor.paymentMode=paymentMode
+return visitor
+} 
+var mail=function(){
+
+}
